22b7721a7aeac65397dd656969fdb35af2ef8086
Yashmahajan23 <yashmahajan0223@gmail.com>
2026-05-28 18:37:01 +0530

    Add files via upload

---
+++ bike.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 4
+            }
+        }
+    })
+
+});
